8e237b3a4741cc17a7d7e7691061357107d8ece7
Jeong YunWon <jeong@youknowone.org>
2013-06-19 02:03:47 +0900

    Implement tintColor hierarchy for UIView

---
+++ UI7Kit/UI7View.h
@@ -0,0 +1,17 @@
+//
+//  UI7View.h
+//  UI7Kit
+//
+//  Created by Jeong YunWon on 13. 6. 19..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import <UI7Kit/UI7Utilities.h>
+
+@interface UIView (UI7Kit)
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < 70000
+@property(nonatomic, retain) UIColor *tintColor;
+#endif
+
+@end
---
+++ UI7Kit/UI7View.m
@@ -0,0 +1,56 @@
+//
+//  UI7View.m
+//  UI7Kit
+//
+//  Created by Jeong YunWon on 13. 6. 19..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import "UI7View.h"
+
+NSMutableDictionary *UI7TintColors = nil;
+
+@implementation UIView (UI7View)
+
++ (void)initialize {
+    if (self == [UIView class]) {
+        if ([UIDevice currentDevice].needsUI7Kit) {
+            UI7TintColors = [[NSMutableDictionary alloc] init];
+            [self addMethodForSelector:@selector(tintColor) fromMethod:[self methodForSelector:@selector(_view_tintColor)]];
+            [self addMethodForSelector:@selector(setTintColor:) fromMethod:[self methodForSelector:@selector(_view_setTintColor:)]];
+            [self copyToSelector:@selector(__view_dealloc) fromSelector:@selector(dealloc)];
+            [self copyToSelector:@selector(dealloc) fromSelector:@selector(_view_dealloc)];
+        }
+    }
+}
+
+- (UIColor *)_view_tintColor {
+    if ([UI7TintColors containsKey:self.pointerString]) {
+        return UI7TintColors[self.pointerString];
+    }
+    if (self.superview) {
+        return self.superview.tintColor;
+    }
+    return [[UI7Kit kit] tintColor];
+}
+
+- (void)_view_setTintColor:(UIColor *)color {
+    if (color) {
+        UI7TintColors[self.pointerString] = color;
+    } else {
+        if ([UI7TintColors containsKey:self.pointerString]) {
+            [UI7TintColors removeObjectForKey:self.pointerString];
+        }
+    }
+}
+
+- (void)__view_dealloc { assert(NO); }
+
+- (void)_view_dealloc {
+    if ([UI7TintColors containsKey:self.pointerString]) {
+        [UI7TintColors removeObjectForKey:self.pointerString];
+    }
+    [self __view_dealloc];
+}
+
+@end
