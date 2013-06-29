86808513305725cb27c954652305032f0d27f35b
Jeong YunWon <jeong@youknowone.org>
2013-06-30 01:50:35 +0900

    Partial implementation of UI7SearchBar

---
+++ UI7Kit/UI7SearchBar.h
@@ -0,0 +1,13 @@
+//
+//  UI7SearchBar.h
+//  UI7Kit
+//
+//  Created by Jeong YunWon on 13. 6. 30..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import <UI7Kit/UI7Utilities.h>
+
+@interface UI7SearchBar : UISearchBar<UI7Patch>
+
+@end
---
+++ UI7Kit/UI7SearchBar.m
@@ -0,0 +1,67 @@
+//
+//  UI7SearchBar.m
+//  UI7Kit
+//
+//  Created by Jeong YunWon on 13. 6. 30..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import "UI7Color.h"
+#import "UI7SearchBar.h"
+
+@implementation UISearchBar (Patch)
+
+- (id)__initWithCoder:(NSCoder *)aDecoder { assert(NO); return nil; }
+- (id)__initWithFrame:(CGRect)frame { assert(NO); return nil; }
+
+- (void)_searchBarInit {
+    CGFloat height = self.frame.size.height;
+    UIGraphicsBeginImageContext(CGSizeMake(1.0, height));
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [[UIColor colorWith8bitWhite:178 alpha:255] set];
+    CGContextFillRect(context, CGRectMake(.0, height - 1.0f, 1.0f, 1.0f));
+    UIImage *backgroundImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    self.backgroundColor = [UIColor colorWith8bitRed:201 green:201 blue:205 alpha:255];
+    self.backgroundImage = backgroundImage;
+    self.placeholder = @" ";
+}
+
+@end
+
+
+@implementation UI7SearchBar
+
++ (void)initialize {
+    if (self == [UI7SearchBar class]) {
+        Class origin = [UISearchBar class];
+
+        [origin copyToSelector:@selector(__initWithCoder:) fromSelector:@selector(initWithCoder:)];
+        [origin copyToSelector:@selector(__initWithFrame:) fromSelector:@selector(initWithFrame:)];
+    }
+}
+
++ (void)patch {
+    Class target = [UISearchBar class];
+
+    [self exportSelector:@selector(initWithCoder:) toClass:target];
+    [self exportSelector:@selector(initWithFrame:) toClass:target];
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [self __initWithFrame:frame];
+    if (self) {
+        [self _searchBarInit];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [self __initWithCoder:aDecoder];
+    if (self != nil) {
+        [self _searchBarInit];
+    }
+    return self;
+}
+
+@end
