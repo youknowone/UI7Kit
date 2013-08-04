3d7666012791ce75d5907e2b1a1ceff1b21bec6c
Jeong YunWon <jeong@youknowone.org>
2013-08-04 21:28:30 +0900

    UI7Switch for catalog

---
+++ UI7KitCatalog/UICSwitchViewController.h
@@ -0,0 +1,15 @@
+//
+//  UICSwitchViewController.h
+//  UI7Kit
+//
+//  Created by Jeong YunWon on 13. 8. 4..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICSwitchViewController : UIViewController
+
+@property(nonatomic,strong) IBOutlet UISwitch *onTintColorSwitch, *thumbTintColorSwitch;
+
+@end
