fd759bd6881a97287708673d0710a8a444c7ca01
Jeong YunWon <jeong@youknowone.org>
2013-09-07 00:25:44 +0900

    popover ready

---
+++ UI7Kit/UI7PopoverController.h
@@ -0,0 +1,13 @@
+//
+//  UI7PopoverController.h
+//  UI7Kit
+//
+//  Created by Jeong YunWon on 13. 9. 6..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import <UI7Kit/UI7Utilities.h>
+
+@interface UI7PopoverController : UIPopoverController<UI7Patch>
+
+@end
---
+++ UI7Kit/UI7PopoverController.m
@@ -0,0 +1,18 @@
+//
+//  UI7PopoverController.m
+//  UI7Kit
+//
+//  Created by Jeong YunWon on 13. 9. 6..
+//  Copyright (c) 2013 youknowone.org. All rights reserved.
+//
+
+#import "UI7PopoverController.h"
+
+
+@implementation UI7PopoverController
+
++ (void)patch {
+
+}
+
+@end
